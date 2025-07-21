ac5ba317229ccc5fa1c87aebeffe6ca86b6ae36f
pcl9556 <pcl9556@naver.com>
2025-07-21 15:57:36 +0900

    0720

---
+++ java_0721/src/main/java/com/projectNull/practice0721/test1.java
@@ -0,0 +1,32 @@
+package com.projectNull.practice0721;
+
+public class test1 {
+
+    public static void main(String[] args) {
+
+        /* 문제 풀자 */
+
+        // 길이가 10인 정수 배열 선언 및 할당
+        int[] arr = new int[10];
+
+        // 배열에 1부터 10까지 값
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        // 출력해
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
+
+
+
+
+
+
+    }
+//    public static String[] fruit() {
+//        String[] fruit = new String[]{"딸기", "바나나", "복숭아", "키위", "사과"};
+
+    }
+
